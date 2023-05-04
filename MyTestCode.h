6c5dcdde2c5dfb90210b3fa81b5964621eb188d3
K <krishna.prasad.k@aptiv.com>
2023-05-04 17:35:43 +0530

    My First commit

---
+++ MyTestCode.h
@@ -0,0 +1,15 @@
+
+
+#ifndef include_MYTESTCOODE_H
+#define include_MYTESTCOODE_H
+
+
+
+
+
+#define DTC_FEATURE_ENABLE_CHK_PARAM_MIN        (uint16_t)0x0000u
+#define ALM_ENABLE                              (uint16_t)0x0001u
+#define HAS_ENABLE                              (uint16_t)0x0002u
+#define DDD_ENABLE                              (uint16_t)0x0004u
+#define AHBC_ENABLE                             (uint16_t)0x0008u
+#define TSI_ENABLE                              (uint16_t)0x0010u
